1042f13558df7b08a7c919ac0fbe0b1b4f7473b9
Jeondanin <39483767+Jeondanin@users.noreply.github.com>
2022-06-24 16:31:47 +0900

    commit for network

--- models/networks.py
+++ models/networks.py
@@ -29,7 +29,7 @@ class BasicConv(nn.Module):
 
 class ChannelPool(nn.Module):
     def forward(self, x):
-        return torch.cat( (torch.max(x,1)[0].unsqueeze(1), torch.mean(x,1).unsqueeze(1)), dim=1 )
+        return torch.cat( (torch.max(x,1)[0].unsqueeze(1), torch.mean(torch.Tensor.float(x),1).unsqueeze(1)), dim=1 )
 
 class SpatialAttn(nn.Module):
     def __init__(self, in_planes, out_planes, kernel_size):
@@ -49,7 +49,7 @@ class Flatten(nn.Module):
         return x.view(x.size(0), -1)
 
 class ChannelAttn(nn.Module):
-    def __init__(self, gate_channels, reduction_ratio=16, pool_types=['avg', 'max']):
+    def __init__(self, gate_channels, reduction_ratio=1, pool_types=['avg', 'max']):
         super(ChannelAttn, self).__init__()
         self.gate_channels = gate_channels
         self.mlp = nn.Sequential(
@@ -90,53 +90,88 @@ class AttentionNetwork(nn.Module):
         self.ms1 = SpatialAttn(2, 1, kernel_size)
         kernel_size = (1, 1)
         self.ms2 = SpatialAttn(2, 1, kernel_size)
-        self.mc = ChannelAttn(gate_channels=32*3)
-        # l is the length of the time window # 3month
+        self.mc = ChannelAttn(gate_channels=2)
+        # l is the length of the time window # 2month
         # w is the dimension of the row vector in the input matrix. 32
         lw1_kernel_size = (3, 32)
         lw2_kernel_size = (5, 32)
         lw3_kernel_size = (10, 32)
         
-        self.convlw1 = BasicConv(1, 32, lw1_kernel_size, stride=1, padding=(1,0), relu=False,  bn=True)
-        self.convlw2 = BasicConv(1, 32, lw2_kernel_size, stride=1, padding=(2,0), relu=False,  bn=True)
-        self.convlw3 = BasicConv(1, 32, lw3_kernel_size, stride=1, padding=(4,0), relu=False,  bn=True)
+        self.convlw1 = BasicConv(1, 1, lw1_kernel_size, stride=1, padding=(1,0), relu=False,  bn=True)
+        self.convlw2 = BasicConv(1, 1, lw2_kernel_size, stride=1, padding=(2,0), relu=False,  bn=True)
+        # self.convlw3 = BasicConv(1, 1, lw3_kernel_size, stride=1, padding=(4,0), relu=False,  bn=True)
         # self.convlw = BasicConv(1, 32, lw_kernel_size, stride=1, padding=0, relu=False,  bn=True)
-        w1_kernel_size = (5, 1)
-        self.convw1 = BasicConv(96, 32, w1_kernel_size, stride=1, padding=(2,0), relu=False,  bn=True)
+        w1_kernel_size = (3, 1)
+        self.convw1 = BasicConv(1*2, 1, w1_kernel_size, stride=1, padding=(1,0), relu=False,  bn=True)
 
     def forward(self, x):
-        x1 = self.ms1(x) # x1.size == x.size as (1, 1, 8760, 32)
-        p1 = self.convlw1(x1) # (1, 32, 8760, 1)
+        x1 = self.ms1(x) # x1.size == x.size as (batch, 1, dayrange, 32)
+        p1 = self.convlw1(x1) # (batch, 32, 1, range)
         p2 = self.convlw2(x1) # (1, 32, 8760, 1)
-        p3 = self.convlw3(x1) # (1, 32, 8760, 1)
-        p3 = torch.cat((p3, torch.Tensor(1, 32, 1, 1)), dim=2)
-        p = torch.cat( (p1, p2, p3), dim=1) # (1, 96, 8760, 1)
+        # p3 = self.convlw3(x1) # (1, 32, 8760, 1)
+        # p3 = torch.cat((p3, torch.Tensor(1, 32, 1, 1)), dim=2)
+        p = torch.cat( (p1, p2), dim=1) # (1, 96, 8760, 1)
         
-        p = self.mc(p) # p = (1, 96, 8760, 1)
-        p = self.ms2(p) # p = (1, 96, 8760, 1)
-        output = self.convw1(p) #  ([1, 32, 8760, 1])
+        p = self.mc(p) # p = ([10, 2, 1, 1])
+        p = self.ms2(p) # p =([10, 2, 1, 1])
+        output = self.convw1(p) # ([10, 32, 1, 1])
         return output
 
 class FACNN(nn.Module):
     def __init__(self):
-        field_dims, embed_dim, mlp_dims, dropout = 1, 2, 3, 4
+        """
+        field_dims : the number of index and time
+            [[t1, t2, ,tm], [i1_1, i1_2, i1_n]]
+            [[t1, t2, ,tm], [i2_1, i2_2, i2_n]]
+            [[t1, t2, ,tm], [i3_1, i3_2, i3_n]]
+            [[t1, t2, ,tm], [i4_1, i4_2, i4_n]]
+            [[t1, t2, ,tm], [i5_1, i5_2, i5_n]]
+            eaach index; may be 33
+        embed_dim : embed次元（適当に）
+        """
+        super().__init__()
+        field_dims, embed_dim, mlp_dims, dropout = tuple(32+i for i in range(32)) ,16, [32, 16, 8], 0.2
         self.fa = DeepFM(field_dims, embed_dim, mlp_dims, dropout)
         self.att_cnn = AttentionNetwork()
         #全結合層
         self.dence = nn.Sequential(
-            nn.Linear('わからない', 64),
+            nn.Linear(2, 64),
             nn.Dropout(p=0.2),
-            nn.Linear(128, 64),
+            nn.Linear(64, 128),
             nn.Dropout(p=0.2),
-            nn.Linear(64, 10),
+            nn.Linear(128, 2),
         )
     
     def forward(self, x):
-        x1 = self.fa(x)
-        x2 = self.att_cnn(x)
+        
+        # x1 = [self.fa(x[i]) for i in range(len(x))]
+
+        # example
+        #######input x shape: (2048, 34) - (batch?, field_dims)
+        #######field_dims: 34
+        #######num_factors: 10
+        #######mlp_dims: [30, 20, 10]
+        ####### self.embedding = nn.Embedding(num_inputs, num_factors) - (3816, 10)
+        #######output x shape: (2048, 1) - (batch, 1)
+        
+        # my case
+        
+        #######field_dims: 32
+        #######num_factors: 16
+        
+        #######output x shape: (1, 1)
+
+        # faのinput shape: (1, 32)
+        x1 = [self.fa(i) for i in x]
+        x2 = self.att_cnn(x.unsqueeze(1).unsqueeze(1)) # [batch, 32, 1, 1]
+        c_x2 = x2.squeeze(2).squeeze(2) # [batch, 32] [10 32]
         # x1 + x2 結合
+        stack_x1 = torch.stack(x1)  # ([10, 1])
+        
+        zx = torch.cat((stack_x1, c_x2), dim=1) # (10,2)
         # full connected layer 
-        # output 
+        output = self.dence(zx)  # 
+        return output
 
 
 
--- tests/test_networks.py
+++ tests/test_networks.py
@@ -2,16 +2,45 @@ import sys
 import unittest
 import torch
 import os
+import numpy as np
 sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
-from models.networks import AttentionNetwork
+from models.networks import AttentionNetwork, DeepFM, FACNN
 
 
-class TestATTCNN(unittest.TestCase):
+# class TestATTCNN(unittest.TestCase):
 
+#     def setUp(self):
+#         # torch.cuda.set_device(0)
+#         self.model = AttentionNetwork()
+#         self.model.eval()
+#         # self.model.cuda(0)
+#         # torch.manual_seed(0)
+#         # # instead of zero init for score tensors use random init
+#         # self.model.score_fr[6].weight.data.random_()
+#         # self.model.score_fr[6].bias.data.random_()
+#         # self.model.score_pool3.weight.data.random_()
+#         # self.model.score_pool3.bias.data.random_()
+#         # self.model.score_pool4.weight.data.random_()
+#         # self.model.score_pool4.bias.data.random_()
+#         self.x = torch.rand((1, 1, 8760, 32))
+        
+
+#     def testForward(self):
+#         print("testForward")
+#         # self.assertEqual(torch.cuda.is_available(), True)
+#         result_foward = self.model.forward(self.x)
+#         # torch.Size([1, 1, 8729, 1])
+#         self.assertEqual(result_foward.shape.numel(), 8760)
+
+class TestDFM(unittest.TestCase):
     def setUp(self):
         # torch.cuda.set_device(0)
-        self.model = AttentionNetwork()
-        self.model.eval()
+        date_range = 100
+        category = 33
+        # ()
+        # self.model = DeepFM((32,) ,embed_dim=16, mlp_dims=(8, 8), dropout=0.2)
+        # # embedding 35, 16
+        
         # self.model.cuda(0)
         # torch.manual_seed(0)
         # # instead of zero init for score tensors use random init
@@ -21,15 +50,22 @@ class TestATTCNN(unittest.TestCase):
         # self.model.score_pool3.bias.data.random_()
         # self.model.score_pool4.weight.data.random_()
         # self.model.score_pool4.bias.data.random_()
-        self.x = torch.rand((1, 1, 8760, 32))
+        self.x = torch.rand((32, 16))
+        # self.embed_info = torch.rand((32, 16))
+        # self.embed_info = torch.tensor([i for i in range(32)])
+
+        self.model = FACNN()
+        self.model.eval()
         
 
     def testForward(self):
         print("testForward")
         # self.assertEqual(torch.cuda.is_available(), True)
-        result_foward = self.model.forward(self.x)
-        # torch.Size([1, 1, 8729, 1])
-        self.assertEqual(result_foward.shape.numel(), 8760)
+        # result_foward = self.model(self.embed_info)
+        batch_num = 10
+        data =  torch.tensor([[1 for i in range(1, 33)]for _ in range(batch_num)])
+        # result_foward = self.model(self.embed_info)
+        result = self.model(data)
 
 
 
